8d6d49db372d800853c91ff8b4e256d53bbfc0a3
Alex Devero <deveroalex@gmail.com>
2017-05-02 08:54:15 +0200

    Implement prop types

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Header from '../../components/Header'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
@@ -6,6 +7,7 @@ import '../../styles/core.scss'
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
     <Header />
+
     <div className='core-layout__viewport'>
       {children}
     </div>
@@ -13,7 +15,7 @@ export const CoreLayout = ({ children }) => (
 )
 
 CoreLayout.propTypes = {
-  children : React.PropTypes.element.isRequired
+  children : PropTypes.element.isRequired
 }
 
 export default CoreLayout
